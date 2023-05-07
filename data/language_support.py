9153c62fbd5c3c198d9e2802e62bebd8ffb43b6d
Francis Conroy <francisc@posteo.net>
2023-05-07 15:23:18 +1000

    Add basic words list

---
+++ data/language_support.py
@@ -0,0 +1,32 @@
+words = {
+    "EN":
+        {
+            1: "one",
+            2: "two",
+            3: "three",
+            4: "four",
+            5: "five",
+            6: "six",
+            7: "seven",
+            8: "eight",
+            9: "nine",
+            10: "ten",
+            11: "eleven",
+            12: "twelve",
+            13: "thirteen",
+            14: "fourteen",  # Just use rule, add digit and teen
+            15: "fifteen",
+            16: "sixteen",  # Just use rule, add digit and teen
+            17: "seventeen",  # Just use rule, add digit and teen
+            18: "eighteen",  # Just use rule, add digit and teen + rule to skip double ts
+            19: "nineteen", # Just use rule, add digit and teen
+            20: "twenty",
+            30: "thirty",
+            40: "forty",
+            50: "fifty",
+            60: "sixty",
+            70: "seventy",
+            80: "eighty",
+            90: "ninety",
+        }
+}
