1aa74bbf7900dce06e8a0983543abb60b8752a08
Francis Conroy <francisc@posteo.net>
2023-05-07 18:27:30 +1000

    Add a basic CLI version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from data.si_units import prefix_lookup
 from util.digits_to_words import three_digits_to_words
 from util.integer_to_thousand_groups import integer_to_thousand_groups
-from data.si_units import prefix_lookup
+
 
 def integer_to_text(integer_in, language="EN"):
     """
@@ -15,9 +16,12 @@ def integer_to_text(integer_in, language="EN"):
         if value == 0:
             continue
         digitlist = list(str(value))
-        suffix = prefix_lookup[language].setdefault((index)*3, "")
+        suffix = prefix_lookup[language].setdefault((index) * 3, "")
         retstr = f"{three_digits_to_words(digitlist, language)} {suffix} {retstr}"
     return retstr.strip()
 
 
-
+if __name__ == "__main__":
+    while True:
+        intstr = input("Enter an integer: ")
+        print(f"In words: \n\t{integer_to_text(int(intstr))}")
